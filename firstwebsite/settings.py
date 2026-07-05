d0c3f71becaf73a6f4dc6e208c75e374dbb67029
Preet Dhiman <preetdhimanr237.com>
2026-07-05 23:19:08 +0530

    deploy

--- firstwebsite/settings.py
+++ firstwebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fy$op47h^uu22g!&jfh&^zd^c8r82h676t(aq#hyf6)1(+&j_o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
