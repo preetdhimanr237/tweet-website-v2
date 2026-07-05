6d43c044b57190691f30667d0dd152226be58c25
Preet Dhiman <preetdhimanr237.com>
2026-07-05 22:39:33 +0530

    deploy

--- firstwebsite/settings.py
+++ firstwebsite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'firstwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_db',
+        'USER': 'tweet_n832_user',
+        'PASSWORD': 'YXtU9MarotdDuVYPMStNN0KRnsVqyI98',
+        'HOST': 'dpg-d958s9rtqb8s73eic57g-a',
+        'PORT': '5432',
     }
 }
 
