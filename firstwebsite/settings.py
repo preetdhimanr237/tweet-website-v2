0458cea0e687a89a8b4b214aabb942733e2abaa3
Preet Dhiman <preetdhimanr237.com>
2026-07-06 10:45:29 +0530

    Connect PostgreSQL

--- firstwebsite/settings.py
+++ firstwebsite/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'firstwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
